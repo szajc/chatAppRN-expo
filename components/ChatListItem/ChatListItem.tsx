b3a890b8ed717790f5ce3bb5a6378aa386aba4bf
szajc <szajc2000@gmail.com>
2021-01-24 19:02:58 +0100

    chat room screen

--- components/ChatListItem/ChatListItem.tsx
+++ components/ChatListItem/ChatListItem.tsx
@@ -1,7 +1,9 @@
 import moment from 'moment';
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Pressable } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import { ChatRoom } from '../../types';
+import { useNavigation } from '@react-navigation/native';
 
 import styles from './style';
 
@@ -12,27 +14,42 @@ export type ChatListItemProps = {
 const ChatListItem = ( props: ChatListItemProps ) => {
     
     const { chatRoom } = props;
+
+    const navigation = useNavigation();
+
     const user = chatRoom.users[1];
     const message = chatRoom.lastMessage.content;
     const time = chatRoom.lastMessage.createdAt;
+    
+    const onClick = () => {
+        navigation.navigate('ChatRoom', { 
+            id: chatRoom.id,
+            name: user.name,
+        });
+        //console.warn(`clicked on ${user.name}`);
+    }
 
     return (
-        <View style={styles.container}>
-            <View style={styles.leftContainer}>
-                <Image 
-                    source={{ uri: user.imageUri }} 
-                    style={styles.avatar} 
-                />
-                <View style={styles.midContainer}>
-                    <Text style={styles.username}>{user.name}</Text>
-                    <Text numberOfLines={1} style={styles.lastMessage}>{message.length>20 ? message.slice(0,17) + "..." : message}</Text>
+        <TouchableWithoutFeedback
+            onPress={onClick} 
+        >
+            <View style={styles.container}>
+                <View style={styles.leftContainer}>
+                    <Image 
+                        source={{ uri: user.imageUri }} 
+                        style={styles.avatar} 
+                    />
+                    <View style={styles.midContainer}>
+                        <Text style={styles.username}>{user.name}</Text>
+                        <Text numberOfLines={1} style={styles.lastMessage}>{message.length>20 ? message.slice(0,17) + "..." : message}</Text>
+                    </View>
                 </View>
+                
+                <Text style={styles.time}>
+                    {moment(time).format('DD/MM/YYYY')}
+                </Text>
             </View>
-            
-            <Text style={styles.time}>
-                {moment(time).format('DD/MM/YYYY')}
-            </Text>
-        </View>
+        </TouchableWithoutFeedback>
     )
 };
 
--- navigation/index.tsx
+++ navigation/index.tsx
@@ -7,11 +7,13 @@ import { ColorSchemeName } from 'react-native';
 //import { Colors } from 'react-native/Libraries/NewAppScreen';
 import Colors from '../constants/Colors';
 
+import ChatRoomScreen from '../screens/ChatRoom';
 import NotFoundScreen from '../screens/NotFoundScreen';
 import { RootStackParamList } from '../types';
 import MainTabNavigator from './MainTabNavigator';
 import LinkingConfiguration from './LinkingConfiguration';
-import { MaterialCommunityIcons, Octicons } from '@expo/vector-icons';
+import { FontAwesome5, MaterialCommunityIcons, MaterialIcons, Octicons } from '@expo/vector-icons';
+
 
 // If you are not familiar with React Navigation, we recommend going through the
 // "Fundamentals" guide: https://reactnavigation.org/docs/getting-started
@@ -60,6 +62,25 @@ function RootNavigator() {
             </View>
           )
         }}/>
+      <Stack.Screen 
+        name="ChatRoom"
+        component={ChatRoomScreen}
+        options={ ({ route }) => ({
+          title: route.params.name,
+          headerRight: () => (
+            <View style={{
+                flexDirection: 'row', 
+                width: 100, 
+                justifyContent: 'space-between',
+                marginRight: 10,
+              }}>
+              <MaterialIcons name="call" size={22} color={'white'} />
+              <FontAwesome5 name="video" size={22} color={'white'} />            
+              <MaterialCommunityIcons name="dots-vertical" size={22} color={'white'} />
+            </View>
+          )
+        })}
+      />
       <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
     </Stack.Navigator>
   );
